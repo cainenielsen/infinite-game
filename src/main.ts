416cbdfdf5ff9acdc3df26076e713ebc351752dd
cainenielsen <caine.nielsen@caineandrebekah.com>
2023-12-30 15:15:24 -0700

    some more clean up

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,10 @@
 // import styles
 import './style.css'
+import { setupDisplay, loadGameData, loadPlayerData } from './steps'
+import { fetchChunksToRender } from './chunks'
+import { startAnimating } from './animation'
 
-// create a new canvas element
-const displayCanvas = document.createElement('canvas')
-
-displayCanvas.style.backgroundColor = 'darkred'
-
-// set the display canvas to the size of the browser window
-displayCanvas.height = window.innerHeight
-displayCanvas.width = window.innerWidth
-
-// add the display canvas to the body
-document.body.appendChild(displayCanvas)
-
-// subscribe to window resize events and keep the display
-// canvas the same size as the screen
-onresize = (event: UIEvent) => {
-  const target = event.target as Window
-  displayCanvas.width = target.innerWidth
-  displayCanvas.height = target.innerHeight
-};
-
-// get the context for the display canvas
-const displayContext = displayCanvas.getContext('2d') as CanvasRenderingContext2D
+const { displayCanvas, displayContext } = setupDisplay()
 
 // some hard coded default properties
 // TODO: make these dynamic
@@ -30,34 +12,8 @@ const gameId = 1
 const playerId = 1
 const mapId = 'default'
 
-// based on the properties above, load the game data from the db
-const game = localStorage.getItem(`game#${gameId}`);
-
-// if a game by that id does not exist, create one and reload the window
-if (game === null) {
-  localStorage.setItem(`game#${gameId}`, JSON.stringify({
-    creator: playerId
-  }));
-
-  location.reload();
-}
-
-// fetch the player data from the db
-const playerDataString = localStorage.getItem(`game#${gameId}#player#${playerId}`)
-
-// if a player does not exist, create it at 0,0 in the db and reload
-if (playerDataString === null) {
-  localStorage.setItem(`game#${gameId}#player#${playerId}`, JSON.stringify({
-    location: {
-      x: 0,
-      y: 0
-    }
-  }));
-
-  location.reload();
-}
-
-const playerData = JSON.parse(playerDataString as string)
+const gameData = loadGameData(gameId, playerId)
+const playerData = loadPlayerData(gameId, playerId)
 
 const chunkSize = 64
 const tileSize = 64
@@ -70,58 +26,11 @@ const renderDistance = 3
 // thew default matrix
 const defaultView = [1, 0, 0, 1, 0, 0];
 
-const view = Array.from(defaultView) as DOMMatrix2DInit;
+const view = Array.from(defaultView);
 
-interface Point {
+export interface Point {
   x: number,
   y: number
-}
-
-// used to fetch coordinates that are siblings of the provided coordinate
-const fetchChunkSiblings = ({ x, y }: Point) => {
-  return [
-    { x, y: y - 1 },
-    { x, y: y + 1 },
-    { x: x - 1, y },
-    { x: x + 1, y }
-  ]
-}
-
-interface Chunk extends Point {
-
-}
-
-// deduplicate coordinates in an array
-const deduplicateChunksToRender = (chunks: Chunk[]) => {
-  const chunkSets = {}
-
-  chunks.forEach((chunk) => {
-    chunkSets[`${chunk.x}|${chunk.y}`] = true
-  })
-
-  return Object.keys(chunkSets).map((chunkSet) => {
-    return { x: Number(chunkSet.split('|')[0]), y: Number(chunkSet.split('|')[1]) }
-  })
-}
-
-// fetch chunk coordinates that should be rendered based on a render distance
-const fetchChunksToRender = (chunk: Chunk, renderDistance: number) => {
-  let chunksToRender = [chunk]
-
-  if (renderDistance > 0) {
-    const siblings = fetchChunkSiblings({ x: chunk.x, y: chunk.y })
-
-    chunksToRender = [...chunksToRender, ...siblings]
-
-    siblings.forEach((sibling) => {
-      const relatives = fetchChunksToRender(sibling, renderDistance - 1)
-
-      chunksToRender = [...chunksToRender, ...relatives]
-    })
-
-  }
-
-  return deduplicateChunksToRender(chunksToRender)
 }
 
 // globally store our game data in memory
@@ -167,10 +76,12 @@ const draw = () => {
     if (chunkData.has(chunkAddress)) {
       // TODO: how to handle when the data exists, but has since been updated
     } else {
-      const chunkDataString = localStorage.getItem(`game#${gameId}#map#${mapId}#${chunkAddress}`)
+      const rawChunkData = localStorage.getItem(`game#${gameId}#map#${mapId}#${chunkAddress}`)
 
       // TODO: generate new data for chunks that are null
-      chunkData.set(chunkAddress, JSON.parse(chunkDataString))
+      if (rawChunkData) {
+        chunkData.set(chunkAddress, JSON.parse(rawChunkData))
+      }
     }
 
     // handle drawing the tiles for each chunk
@@ -224,39 +135,9 @@ const draw = () => {
   displayContext.fillRect(playerVisualAddress.x, playerVisualAddress.y, tileSize, tileSize)
 }
 
-const stop = false;
-let frameCount = 0;
-let fpsInterval: number, start: number, then: number, elapsed;
-
-// kick off an animation with specific conditions
-const startAnimating = (fps: number) => {
-  fpsInterval = 1000 / fps;
-  then = window.performance.now();
-  start = then;
-  animate(then);
-}
-
-// the animation loop
-const animate = (timestamp: number) => {
-  if (stop) return
-
-  requestAnimationFrame(animate);
-
-  elapsed = timestamp - then;
-
-  if (elapsed > fpsInterval) {
-    then = timestamp - (elapsed % fpsInterval);
-
-    draw()
-
-    const sinceStart = timestamp - start;
-    const currentFps = Math.round(1000 / (sinceStart / ++frameCount) * 100) / 100;
-
-    // console.info(currentFps)
-  }
-}
-
-startAnimating(60);
+startAnimating(() => {
+  draw()
+});
 
 // handling player controls
 const controls = {
---
+++ src/steps.ts
@@ -0,0 +1,59 @@
+export const setupDisplay = () => {
+  // create a new canvas element
+  const displayCanvas = document.createElement('canvas')
+
+  displayCanvas.style.backgroundColor = 'darkred'
+
+  // set the display canvas to the size of the browser window
+  displayCanvas.height = window.innerHeight
+  displayCanvas.width = window.innerWidth
+
+  // add the display canvas to the body
+  document.body.appendChild(displayCanvas)
+
+  // subscribe to window resize events and keep the display
+  // canvas the same size as the screen
+  onresize = (event: UIEvent) => {
+    const target = event.target as Window
+    displayCanvas.width = target.innerWidth
+    displayCanvas.height = target.innerHeight
+  };
+
+  // get the context for the display canvas
+  const displayContext = displayCanvas.getContext('2d') as CanvasRenderingContext2D
+
+  return { displayCanvas, displayContext }
+}
+
+export const loadGameData = (gameId: number, playerId: number) => {
+  // based on the properties above, load the game data from the db
+  const rawGameData = localStorage.getItem(`game#${gameId}`);
+
+  // if a game by that id does not exist, create one and reload the window
+  if (rawGameData === null) {
+    localStorage.setItem(`game#${gameId}`, JSON.stringify({
+      creator: playerId
+    }));
+
+    return location.reload();
+  }
+  return JSON.parse(rawGameData)
+}
+
+export const loadPlayerData = (gameId: number, playerId: number) => {
+  // fetch the player data from the db
+  const rawPlayerData = localStorage.getItem(`game#${gameId}#player#${playerId}`)
+
+  // if a player does not exist, create it at 0,0 in the db and reload
+  if (rawPlayerData === null) {
+    localStorage.setItem(`game#${gameId}#player#${playerId}`, JSON.stringify({
+      location: {
+        x: 0,
+        y: 0
+      }
+    }));
+
+    return location.reload();
+  }
+  return JSON.parse(rawPlayerData)
+}
